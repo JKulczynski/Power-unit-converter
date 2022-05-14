1ab3bfca70d0b0dcd16588041de68f5c260b5c19
JKulczynski <j.kulczynski@adform.com>
2022-05-14 02:54:30 +0200

    4th commit of app. Changed `if` to `pattern matching`. Still working on.

--- src/main/scala/powerUnitConverter.scala
+++ src/main/scala/powerUnitConverter.scala
@@ -6,42 +6,33 @@ object powerUnitConverter extends App {
   val title = "Konwerter jednostek"
   println(title)
 
-  while (true) {
+  val choice = readLine("Podaj co chcesz przeliczyć (Km/KW), (Kw/Km), (Nm/LbFt), (LbFt/Nm): ")
 
-    val choice = readLine("Podaj co chcesz przeliczyć (Km/KW), (Kw/Km), (Nm/LbFt), (LbFt/Nm): ")
-    if (choice == "kw") {
-      //Konwersja KW na Km
-      val kw = readLine("Podaj wartość Kw: ").toInt
-      val kwtoKm = KwToKm(kw)
-      val kwResult = "Km: " + kwtoKm.conversion
-      println(kwResult)
-    } else if (choice == "km") {
-      //Konwersja Km na KW
-      val km = readLine("Podaj wartość Km: ").toInt
-      val kmToKw = KmToKw(km)
-      val kmResult = "KW: " + kmToKw.conversion
-      println(kmResult)
-    } else if (choice == "nm") {
-      //konwersja Nm na Lb/ft
-      val nm = readLine("Podaj wartość Nm: ").toInt
-      val nmtolb = NmToLbft(nm)
-      val nmResult = "Lb/ft: " + nmtolb.conversion
-      println(nmResult)
-    } else if (choice == "lbft") {
-      //konwersja Lb/ft na Nm
-      val lbft = readLine("Podaj wartość Lb/ft: ").toInt
-      val lbfttonm = LbftToNm(lbft)
-      val lbResult = "Nm: " + lbfttonm.conversion
-      println(lbResult)
-    } else {
-      println("Nieprawidłowy wybór")
+    choice match {
+      case "kw" => val kw = readLine("Podaj wartość Kw: ").toInt
+        val kwtoKm = KwToKm(kw)
+        val kwResult = "Km: " + kwtoKm.conversion
+        println(kwResult)
+      case "km" => val km = readLine("Podaj wartość Km: ").toInt
+        val kmToKw = KmToKw(km)
+        val kmResult = "KW: " + kmToKw.conversion
+        println(kmResult)
+      case "nm" => val nm = readLine("Podaj wartość Nm: ").toInt
+        val nmtolb = NmToLbft(nm)
+        val nmResult = "Lb/ft: " + nmtolb.conversion
+        println(nmResult)
+      case "lbft" => val lbft = readLine("Podaj wartość Lb/ft: ").toInt
+        val lbfttonm = LbftToNm(lbft)
+        val lbResult = "Nm: " + lbfttonm.conversion
+        println(lbResult)
+      case _ => println("Nieprawidłowy wybór")
     }
 
-    val odp = readLine("Czy chcesz kontynuować? (t/n)")
+  val odp = readLine("Czy chcesz kontynuować? (t/n)")
 
-    if (odp == "n") {
-      println("Dziękuję za użycie")
+  odp match {
+    case "t" => choice
+    case "n" => println("Dziękuję za użycie")
       exit(0)
-    }
   }
 }
